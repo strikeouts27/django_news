e7cbd7c83acb1cd4f039e56b86ff2ff5a0b7cb7f
strikeouts27 <stribling.andrew@gmail.com>
2023-09-07 14:36:39 -0500

    corrected the +New Button

--- articles/urls.py
+++ articles/urls.py
@@ -14,7 +14,5 @@ urlpatterns = [
     path("new/", ArticleCreateView.as_view(), name="article_new"),
     path("", ArticleListView.as_view(), name="article_list"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
-   
-    
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
 ]
