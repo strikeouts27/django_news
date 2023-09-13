efe440a9a34efc86a984c19ee9df7c2c36d088e0
strikeouts27 <stribling.andrew@gmail.com>
2023-09-13 04:12:30 -0500

    added div to aritcle_detail to include edit and delete options

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = True # env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["strikeouts27news.fly.dev", "localhost", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://fly.io/apps/strikeouts27news"]
