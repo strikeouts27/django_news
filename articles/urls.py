6852a2298410b48b5bc47b5bcecb71cd92a58d97
strikeouts27 <stribling.andrew@gmail.com>
2023-09-13 05:19:43 -0500

    fixed if logic for aritcle_detial

--- articles/urls.py
+++ articles/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
-    path("", ArticleListView.as_view(), name="article_list"),
     path("new/", ArticleCreateView.as_view(), name="article_new"),
+    path("", ArticleListView.as_view(), name="article_list"),  
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
 ]
 """
--- articles/views.py
+++ articles/views.py
@@ -28,20 +28,25 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
         context["form"] = CommentForm() 
         return context 
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
-    model = Article 
-    template_name = "article_delete.html"
-    success_url = reverse_lazy("article_list")
-
-class ArticleCreateView(LoginRequiredMixin, CreateView):
-    model = Article 
-    template_name = "article_new.html"
-    fields = ("title", "body", )
-
-    def form_valid(self,form):
-        form.instance.author = self.request.user 
-        return super().form_valid(form) 
+    def get(self, request, *args, **kwargs): 
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
     
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object 
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        article = self.object
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
@@ -54,7 +59,15 @@ class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         obj = self.get_object()
         return obj.author == self.request.user
 
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article 
+    template_name = "article_new.html"
+    fields = ("title", "body", )
 
+    def form_valid(self,form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form) 
+    
 
 class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
@@ -109,22 +122,4 @@ class CommentPost(SingleObjectMixin, FormView):
         article = self.object
         return reverse("article_detail", kwargs={"pk": article.pk})
     
-class ArticleDetailView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs): 
-        view = CommentGet.as_view()
-        return view(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        view = CommentPost.as_view()
-        return view(request, *args, **kwargs)
 
-    def form_valid(self, form):
-        comment = form.save(commit=False)
-        comment.article = self.object 
-        comment.author = self.request.user
-        comment.save()
-        return super().form_valid(form)
-    
-    def get_success_url(self):
-        article = self.object
-        return reverse("article_detail", kwargs={"pk": article.pk})
